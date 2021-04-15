578ef65e49f6552d5e1d036cf5959f85d0e62bba
Daniel Pape <danielwpape@gmail.com>
2021-04-15 14:11:59 +0100

    Update ContentView.swift

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var wakeUp = Date()
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                Text("When would you like to wake up?")
+                    .font(.headline)
+                DatePicker("Pick a date",
+                           selection: $wakeUp,
+                           displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+
+                Stepper(value: $sleepAmount, in: 4...12, step: 0.25){
+                    Text("\(sleepAmount,specifier: "%g") hours")
+                }.padding()
+                
+                Text("Amount of Coffee")
+                    .font(.headline)
+
+                Stepper(value: $coffeeAmount, in: 1...20, step: 1){
+                    Text("\(coffeeAmount) cup\(coffeeAmount == 1 ? "" : "s")")
+                }.padding()
+            }
+            .navigationTitle("Better Rest")
+            .navigationBarItems(trailing:
+                                    Button(action:calculateBedTime){
+                    Text("Calculate")
+                }
+            )
+        }
+    }
+    
+    func calculateBedTime(){
+        
     }
 }
 
